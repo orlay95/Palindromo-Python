48592bee89dc96b0ec9bf5a76743137f34e3e9e5
Orlay Andrés Molina Gomez <96931645+orlay95@users.noreply.github.com>
2022-06-18 19:37:57 -0500

    Add files via upload
    Código Palindromo en Python

---
+++ palindromo.py
@@ -0,0 +1,21 @@
+def palindromo(frase):
+    frase = frase.replace(' ', '')
+    frase = frase.lower()
+    frase_invertidad =  frase[::-1]
+    if frase == frase_invertidad:
+        return True
+    else:
+        return False
+
+
+def run():
+    frase = input('Escribe una frase: ')
+    comprobar = palindromo(frase)
+    if comprobar == True:
+        print("La frase es Palindromo.")
+    else:
+        print("La frase no es Palindromo.")
+        
+
+if __name__ == '__main__':
+    run()
